bede1201e6c4350878b4435ed4c3d706529f311b
Andrew Lubanyana <100496410+Andrewlubanyana@users.noreply.github.com>
2022-05-06 17:42:26 +0200

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get("/bmiCalculator", (req, res) => {
+
+    res.sendFile(__dirname + "/bmiCalculator.html");
+
+});
+
+app.post("/bmiCalculator", (req, res) => {
+
+    var Weight = parseFloat(req.body.Weight);
+    var Height = parseFloat(req.body.Height);
+
+    var bmi = Math.round(Weight / (Height * Height));
+
+
+    res.send("Your BMI Calculation is " + bmi);
+});
+
+app.listen(3000,function(){
+  console.log("Server is working")
+});
